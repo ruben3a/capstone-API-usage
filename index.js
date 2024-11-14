b66f0e55b800e40c4adb52a70e649044ac7429ba
ruben <rubenjosearaujo@icloud.com>
2024-11-14 19:30:27 +0000

    got the random manga cover image displayed, nex is the title, and after that the first chapter

--- index.js
+++ index.js
@@ -8,14 +8,35 @@ const APIurl = "https://api.mangadex.org";
 
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get(APIurl + "/manga");
-    console.log(response.data.result);
-    console.log(response.data);
-    res.send(response.data.data);
+    const response = await axios.get(APIurl + "/manga/random");
+    // console.log(response.data);
+    const mangaID = response.data.data.id;
+    const rel = response.data.data.relationships;
+    // console.log(rel);
+    const coverArt = rel.find((coverArt) => coverArt.type === "cover_art");
+    const coverArtID = coverArt.id;
+    const CoverFileName = await GetCoverFileName(coverArtID);
+    const coverArtUrl =
+      "https://uploads.mangadex.org/covers/" +
+      mangaID +
+      "/" +
+      CoverFileName +
+      ".512.jpg";
+    res.render("index.ejs", { cover: coverArtUrl });
   } catch (error) {
-    console.log(error.data.data);
+    console.log(error.data);
   }
 });
+
+async function GetCoverFileName(coverArtID) {
+  try {
+    const result = await axios.get(APIurl + "/cover/" + coverArtID);
+    const CoverFileName = result.data.data.attributes.fileName;
+    return CoverFileName;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 app.listen(port, () => {
   console.log("on");
